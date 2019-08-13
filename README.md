413d4b4c149a776ba9c1d687076390b89266dfd8
Panna <pavanksshetty@gmail.com>
2019-08-13 10:36:46 -0700

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-#Portfolio
-
-### Things to install
-
-` npm install --save react-mdl`
-
-`  npm install --save react-router-dom`
-
-https://uigradients.com
---
+++ precache-manifest.aa768e9e630c3f8a58470c1b3f81269a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "044996eaa31f794e6f398190f39b86ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ee1fd7fb827a530facd",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "3449aa1a3b9225784709",
+    "url": "/static/css/main.12b8baac.chunk.css"
+  },
+  {
+    "revision": "6ee1fd7fb827a530facd",
+    "url": "/static/js/2.77f3d42b.chunk.js"
+  },
+  {
+    "revision": "3449aa1a3b9225784709",
+    "url": "/static/js/main.2496e4f6.chunk.js"
+  },
+  {
+    "revision": "b732ca256bf8411160ec",
+    "url": "/static/js/runtime~main.55ec1482.js"
+  },
+  {
+    "revision": "7834a0cd33bf75b1e14ecf86524e8bf9",
+    "url": "/static/media/profile.7834a0cd.jpg"
+  }
+]);
