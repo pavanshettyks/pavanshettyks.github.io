8d34d8c3927fc6f27e7bcedf2341bfa46a9d716b
Panna <pavanksshetty@gmail.com>
2019-08-25 12:38:10 -0700

    Updates

---
+++ precache-manifest.5ab93edbe1cd598b1f87b44849538065.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d86138f7c51470b017ff04dbaf1484",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50992cfc2294bfb9d7d9",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "1eb14a7c361fc3e9320b",
+    "url": "/static/css/main.c1cd5814.chunk.css"
+  },
+  {
+    "revision": "50992cfc2294bfb9d7d9",
+    "url": "/static/js/2.2bb7298d.chunk.js"
+  },
+  {
+    "revision": "1eb14a7c361fc3e9320b",
+    "url": "/static/js/main.35ba1829.chunk.js"
+  },
+  {
+    "revision": "b732ca256bf8411160ec",
+    "url": "/static/js/runtime~main.55ec1482.js"
+  },
+  {
+    "revision": "e4bf3aca639c5dc7680504b784409fb3",
+    "url": "/static/media/LogoPython.e4bf3aca.png"
+  },
+  {
+    "revision": "68b835f86d9402568aa41a2946798246",
+    "url": "/static/media/LogoReact.68b835f8.png"
+  },
+  {
+    "revision": "5499d0ef399def6e869c74dd5607a6a7",
+    "url": "/static/media/LogoReactNative.5499d0ef.jpg"
+  },
+  {
+    "revision": "0e6760ae72215cc280f39225f783524c",
+    "url": "/static/media/Pavan_Kumar_Resume.0e6760ae.pdf"
+  },
+  {
+    "revision": "7834a0cd33bf75b1e14ecf86524e8bf9",
+    "url": "/static/media/profile.7834a0cd.jpg"
+  }
+]);
