daac5fbcb659723cd717a96a241fac5aea3f4bec
Panna <pavanksshetty@gmail.com>
2019-08-25 01:16:18 -0700

    early build

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {ProgressBar} from 'react-mdl'
 
 export default function AboutMe() {
     return (
@@ -18,10 +19,33 @@ export default function AboutMe() {
                         <h4>SAP Labs India</h4>
                         <p>2015 - 2018</p>
                         <h5>Worked on SAP ABAP, SAP Fiori Smart Template. Developed API's for 3rd party Intergration.
-                        Specialization in Business Partner and Material Master area</h5>
+                        Specialization in Business Partner and Material Master area.</h5>
             </div>
             <div id="skills">
                     <h2>Skills</h2>
+                    <hr />
+                    <div id="skill">
+                            <h5>Python</h5> <ProgressBar style={{margin: 'auto', width: '75%', height: '15px'}} progress={92} />
+                    </div>
+                    <div id="skill">
+                            <h5>React</h5> <ProgressBar style={{margin: 'auto', width: '75%', height: '15px'}} progress={87} />
+                    </div>
+                    <div id="skill">
+                            <h5>React Native</h5> <ProgressBar style={{margin: 'auto', width: '75%', height: '15px'}} progress={88} />
+                    </div>
+                    <div id="skill">
+                            <h5>SAP ABAP</h5> <ProgressBar style={{margin: 'auto', width: '75%', height: '15px'}} progress={84} />
+                    </div>
+                    <div id="skill">
+                            <h5>Javascript</h5> <ProgressBar style={{margin: 'auto', width: '75%', height: '15px'}} progress={80} />
+                    </div>
+                    <div id="skill">
+                            <h5>Java</h5> <ProgressBar style={{margin: 'auto', width: '75%', height: '15px'}} progress={70} />
+                    </div>
+                    <div id="skill">
+                            <h5>HTML, CSS</h5> <ProgressBar style={{margin: 'auto', width: '75%', height: '15px'}} progress={80} />
+                    </div>
+            
             </div>
 
         </div>
