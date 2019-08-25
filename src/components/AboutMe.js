6d1b08a243d261452f8d5e1d764a67fb7bb8472c
Panna <pavanksshetty@gmail.com>
2019-08-25 00:34:56 -0700

    ABout me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,7 +3,27 @@ import React from 'react'
 export default function AboutMe() {
     return (
         <div id="aboutme">
-            AboutMe
+            <div id= "education"> 
+                    
+                    <h2>Education</h2>
+                    <hr />
+                        <h4>California State University, Fullerton</h4>
+                        <p>2018 - Present</p>
+                        <h5>Master's in Computer Science</h5>
+                        <h4>Visvesvaraya Technological University, India</h4>
+                        <p>2011 - 2015</p>
+                        <h5>Bachelors's in Information Science & Engineering </h5>
+                    <h2>Experience</h2>
+                    <hr />
+                        <h4>SAP Labs India</h4>
+                        <p>2015 - 2018</p>
+                        <h5>Worked on SAP ABAP, SAP Fiori Smart Template. Developed API's for 3rd party Intergration.
+                        Specialization in Business Partner and Material Master area</h5>
+            </div>
+            <div id="skills">
+                    <h2>Skills</h2>
+            </div>
+
         </div>
     )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ import LandingPage from './LandingPage';
 import ContactMe from './ContactMe';
 import AboutMe from './AboutMe';
 import Projects from './Projects'
-import Skills from './Skills'
+
 
 export default function Main() {
     return (
